56e6ec56f14fe7126534c1ee03626f42192e2f2c
Levid <jassontejada@gmail.com>
2024-05-03 22:56:24 -0400

    Utilizando el componente direcciones en mi app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ClientesComponent } from './clientes/clientes.component';
+import { DireccionComponent } from './direccion/direccion.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ClientesComponent],
+  imports: [RouterOutlet, ClientesComponent, DireccionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
