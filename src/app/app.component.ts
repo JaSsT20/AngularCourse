e4b9c362f47cc0b67f9d072c0e9521a9a83dea4c
Levid <jassontejada@gmail.com>
2024-05-03 22:38:06 -0400

    Usando componente clientes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ClientesComponent } from './clientes/clientes.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ClientesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
